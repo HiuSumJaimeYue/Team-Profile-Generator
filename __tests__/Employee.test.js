0b3707ea53103c505beabd6e44b2ac0acb484ad0
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-23 05:17:20 -0400

    test and build Employee object and methods

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require('../lib/Employee');
+
+test('creates a employee object', () => {
+    const employee = new Employee('A', 'email.com');
+
+    expect(employee.name).toBe('A');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('A', 'email.com');
+
+    expect(employee.getName()).toBe(employee.name);
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('A', 'email.com');
+
+    expect(employee.getId()).toBe(employee.id);
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('A', 'email.com');
+
+    expect(employee.getEmail()).toBe(employee.email);
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('A', 'email.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
+
