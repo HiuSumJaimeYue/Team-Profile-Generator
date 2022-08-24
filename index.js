9d7d5dcb49c66ac1dbe109ca52dca8e1dac12f0b
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-23 21:23:18 -0400

    Team Manager Prompt

--- index.js
+++ index.js
@@ -3,45 +3,129 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-function createEmployee() {
+const createEmployee = (portfolioData = []) => {
+    console.log(`
+    ====================
+    Add another Employee
+    ====================
+    `);
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.members) {
+        portfolioData.members = [];
+    }
+
     inquirer
-    .prompt([{
-        type: 'text',
-        name: 'name',
-        message: 'What is the name of the employee?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter a name!');
-                return false;
+        .prompt([{
+            type: 'list',
+            message: 'Add what role to the team?',
+            name: 'role',
+            choices: ['Engineer', 'Intern']
+        }, {
+            type: 'text',
+            name: 'name',
+            message: 'What is the name of the employee?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the email of the employee? (Required)',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please enter an email!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the employee? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!');
+                    return false;
+                }
             }
-        }
-    }])
-    .then(employeeData => {
-        console.log(employeeData);
-        // this.employee = new Manager(name, email);
+        }, {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
+        }])
+        .then(employeeData => {
+            console.log(employeeData);
+            portfolioData.members.push(employeeData);
+            // this.employee = new Manager(name, email);
 
-        // console.log(this.employee);
-        // this.createHTML();
+            // console.log(this.employee);
+            if (employeeData.confirmAddEmployee) {
+                return createEmployee(portfolioData);
+            } else {
+                console.log(portfolioData);
+                return portfolioData;
+            }
+            // this.createHTML();
 
-    });
+        });
 }
 
-createEmployee();
+const createTeam = (teamData = []) => {
+    console.log(`
+    ========================
+    Team Manager Information
+    ========================
+    `);
+
+    inquirer
+        .prompt([{
+            type: 'text',
+            name: 'name',
+            message: 'What is the name of the team manager?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the the team manager? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'officeNum',
+            message: 'What is the office number of the the team manager? (Required)',
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an office number!');
+                    return false;
+                }
+            }
+        }])
+        .then(managerData => {
+            teamData.manager = managerData;
+            // this.employee = new Manager(name, email);
+            console.log(teamData);
+            // console.log(this.employee);
+            createEmployee(teamData);
+            // this.createHTML();
+
+        });
+}
+
+createTeam();
 
 
 
